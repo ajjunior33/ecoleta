31e42af29cbb8bf28bf5f10b2e997288bf83f2a0
André Junior <andrejr33@suporteava.com.br>
2020-06-04 22:14:46 -0300

    Aula 03 - Final

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -56,20 +56,20 @@ for(let item of itemsToCollect){
     item.addEventListener("click", handleSelectedItem);
 }
 const collectedItems = document.querySelector("input[name=items]");
+
 let selectedItems = [];
 
 function handleSelectedItem(event){
     const itemLi = event.target;
     itemLi.classList.toggle("selected");
-    const {id} = itemLi.dataset;
+    const itemId = itemLi.dataset.id;
 
-    const alreadySelected = selectedItems.findIndex(item => item == itemId);
-    /*
-        const alreadySelected = selectedItems.findIndex((item) => {
-            const itemFound = item == itemId
-            return itemFound;
-        });
-    */
+    //const alreadySelected = selectedItems.findIndex(item => item == itemId);
+    const alreadySelected = selectedItems.findIndex((item) => {
+        const itemFound = item == itemId
+        return itemFound;
+    });
+    
 
     if(alreadySelected >= 0){
         const filteredItems = selectedItems.filter(item => {
